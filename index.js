b6857b07450e9e927e15e5a05847de805e690569
nayoung4089 <un19991217@gmail.com>
2021-07-07 18:44:45 +0900

    교육과정 부분 정리

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var toggleBtn = document.querySelector('.navbar__toggleBtn');
-var toggleBtn = document.querySelector('.navbar__toggleBtn');
 var menu = document.querySelector('.navbar__menu');
 var icons = document.querySelector('.navbar__icons');
 
@@ -9,26 +8,26 @@ toggleBtn.addEventListener('click', ()=>{
 });
 
 
-var first = document.getElementById("first");
-var second =document.getElementById("second");
-var third = document.getElementById("third");
-var defirst = document.querySelector('.details-first');
-var desecond = document.querySelector('.details-second');
-var dethird = document.querySelector('.details-third');
+// var first = document.getElementById("first");
+// var second =document.getElementById("second");
+// var third = document.getElementById("third");
+// var defirst = document.querySelector('.details-first');
+// var desecond = document.querySelector('.details-second');
+// var dethird = document.querySelector('.details-third');
 
-first.addEventListener("click", function(){
-    defirst.style.display="block";
-    desecond.style.display="none";
-    dethird.style.display="none";
+// first.addEventListener("click", function(){
+//     defirst.style.display="block";
+//     desecond.style.display="none";
+//     dethird.style.display="none";
 
-});
-second.addEventListener("click", function(){
-    desecond.style.display="block";
-    dethird.style.display="none";
-    defirst.style.display="none";
-});
-third.addEventListener("click", function(){
-    dethird.style.display="block";
-    desecond.style.display="none";
-    defirst.style.display="none";
-});
+// });
+// second.addEventListener("click", function(){
+//     desecond.style.display="block";
+//     dethird.style.display="none";
+//     defirst.style.display="none";
+// });
+// third.addEventListener("click", function(){
+//     dethird.style.display="block";
+//     desecond.style.display="none";
+//     defirst.style.display="none";
+// });
